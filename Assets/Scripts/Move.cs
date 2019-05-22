4d9842fd94bf0a260e7f407ad93a9f5a473fa9bb
Max <mjadeb7@gmail.com>
2019-05-22 07:38:38 -0700

    TestPush

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public int Speedmin = 0;
+    public float Speedmax = 20;
+
+    int speed;
+    void Start()
+    {
+        //speed = UnityEngine.Random.Range(Speedmin, Speedmax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-Vector3.forward*0.001f, Space.Self);
+    }
+}
---
+++ Assets/Scripts/RespawnEnemys.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnEnemys : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float RangeMinX = -15;
+    public float RangeMaxX = 15;
+    public Vector3 SpawnDistFromPlayer;
+
+    public int AmountofEnemys = 0;
+    public int RespawnTime = 5;
+    public GameObject[] Resp;
+    public GameObject Enemy;
+
+    
+    void Start()
+    {
+        Resp = new GameObject[AmountofEnemys];
+        for (int i = 0; i < AmountofEnemys; i++)
+        {
+            SpawnDistFromPlayer.x = UnityEngine.Random.Range(RangeMinX, RangeMaxX);
+            SpawnDistFromPlayer.y = UnityEngine.Random.Range(-5, 5);
+            GameObject go = Instantiate(Enemy, SpawnDistFromPlayer, Quaternion.identity) as GameObject;
+            go.transform.localScale = Vector3.one;
+            Resp[i] = go;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        int i = 0;
+        while(i < AmountofEnemys)
+        {
+            Respawn(i);
+            i++;
+        }
+        
+    }
+    IEnumerator Wait(int RespawnTime, int i)
+    {
+        print(Time.time);
+        yield return new WaitForSecondsRealtime(5);
+        Respawn(i);
+    }
+    public void Respawn(int i)
+    {
+        if(Resp[i].activeSelf == false)
+        {
+            SpawnDistFromPlayer.x = UnityEngine.Random.Range(RangeMinX, RangeMaxX);
+            SpawnDistFromPlayer.y = UnityEngine.Random.Range(-5, 5);
+            Resp[i].transform.position = SpawnDistFromPlayer;
+            Resp[i].SetActive(true);
+        }
+        
+        
+    }
+
+}
