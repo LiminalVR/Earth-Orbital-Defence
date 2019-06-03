cc1f8ab6ef21769ec5f34bc6bc35be5e4ed1d49c
Max <mjadeb7@gmail.com>
2019-06-02 22:31:50 -0700

    Worked on Timer and Still working on Respawn

--- Assets/Scripts/RespawnEnemys.cs
+++ Assets/Scripts/RespawnEnemys.cs
@@ -7,40 +7,55 @@ using UnityEngine;
 public class RespawnEnemys : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float RangeMinX = -15;
-    public float RangeMaxX = 15;
+
+
     public Vector3 SpawnDistFromPlayer;
 
-   // public int AmountofEnemys = 0;
-    public int RespawnTime = 5;
+    // public int AmountofEnemys = 0;
+
+    public WaveTimer Timer;
     public GameObject[] Resp;
     public GameObject Debri;
     public GameObject Rock;
     public GameObject Boi;
+
     public int MAXNUMOFDEBRI = 0;
     public int MAXNUMOFROCK = 0;
     public int MAXNUMOFGREENBOI = 0;
-    int max;
     public int StartingTime;
+    public int AmountDebriAddedAfterTime;
+    public int AmountRockAddedAfterTime;
+    public int AmountAlienAddedAfterTime;
     public int startingAmtDebri;
     public int startingAmtRock;
     public int startingAmtBoi;
-    float timer = 0.0f;
+    private int lastDebriSpawned;
+    private int lastRockSpawned;
+    private int lastAlienSpawned;
+    private int max;
+    public float timeToWaitTillSpawnIncreaseDebri;
+    public float timeToWaitTillSpawnIncreaseRock;
+    public float timeToWaitTillSpawnIncreaseAlien;
+    private float timerDebri;
+    private float timerRock;
+    private float timerAlien;
 
 
     void Start()
     {
+
         max = MAXNUMOFDEBRI + MAXNUMOFROCK + MAXNUMOFGREENBOI;
         Resp = new GameObject[max];
         for (int i = 0; i < MAXNUMOFDEBRI; i++)
         {
             Vector2 Spawn = UnityEngine.Random.insideUnitCircle.normalized;
-            SpawnDistFromPlayer.x =  Spawn.x * 2400;
-            SpawnDistFromPlayer.z = Spawn.y * 2400 - ( 376); 
+            SpawnDistFromPlayer.x = Spawn.x * 2400;
+            SpawnDistFromPlayer.z = Spawn.y * 2400 - (376);
             GameObject go = Instantiate(Debri, SpawnDistFromPlayer, Quaternion.identity) as GameObject;
-            
+
             Resp[i] = go;
             Resp[i].SetActive(false);
+            lastDebriSpawned = i;
         }
         for (int i = MAXNUMOFDEBRI; i < max; i++)
         {
@@ -51,8 +66,9 @@ public class RespawnEnemys : MonoBehaviour
 
             Resp[i] = go;
             Resp[i].SetActive(false);
+            lastRockSpawned = i;
         }
-        for (int i = MAXNUMOFROCK + MAXNUMOFDEBRI; i < max ; i++)
+        for (int i = MAXNUMOFROCK + MAXNUMOFDEBRI; i < max; i++)
         {
             Vector2 Spawn = UnityEngine.Random.insideUnitCircle.normalized;
             SpawnDistFromPlayer.x = Spawn.x * 2400;
@@ -61,15 +77,18 @@ public class RespawnEnemys : MonoBehaviour
 
             Resp[i] = go;
             Resp[i].SetActive(false);
+            lastAlienSpawned = i;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        timer += Time.deltaTime;
-        if(timer >= StartingTime)
+
+        timerDebri += Time.deltaTime;
+        timerRock += Time.deltaTime;
+        timerAlien += Time.deltaTime;
+        if (Timer.getTime() <= StartingTime)
         {
             for (int i = 0; i < startingAmtDebri; i++)
             {
@@ -83,7 +102,7 @@ public class RespawnEnemys : MonoBehaviour
 
                 }
             }
-            for (int i = MAXNUMOFDEBRI; i < MAXNUMOFDEBRI+startingAmtRock; i++)
+            for (int i = MAXNUMOFDEBRI; i < MAXNUMOFDEBRI + startingAmtRock; i++)
             {
                 if (!Resp[i].activeInHierarchy)
                 {
@@ -95,7 +114,7 @@ public class RespawnEnemys : MonoBehaviour
 
                 }
             }
-            for (int i = MAXNUMOFDEBRI+ MAXNUMOFROCK; i < MAXNUMOFDEBRI+MAXNUMOFROCK+startingAmtBoi; i++)
+            for (int i = MAXNUMOFDEBRI + MAXNUMOFROCK; i < MAXNUMOFDEBRI + MAXNUMOFROCK + startingAmtBoi; i++)
             {
                 if (!Resp[i].activeInHierarchy)
                 {
@@ -108,20 +127,71 @@ public class RespawnEnemys : MonoBehaviour
                 }
             }
         }
-        
+        if (timerDebri >= timeToWaitTillSpawnIncreaseDebri)
+        {
+            int size = lastDebriSpawned + AmountDebriAddedAfterTime;
+
+            if (size > MAXNUMOFDEBRI)
+                size = MAXNUMOFDEBRI - 1;
+            for (int i = lastDebriSpawned; i < size; i++)
+            {
+                if (!Resp[i].activeInHierarchy)
+                {
+                    Resp[i].SetActive(true);
+                    Vector2 Spawn = UnityEngine.Random.insideUnitCircle.normalized;
+                    SpawnDistFromPlayer.x = Spawn.x * 1200;
+                    SpawnDistFromPlayer.z = Spawn.y * 1200 - (376);
+                    Resp[i].transform.position = SpawnDistFromPlayer;
+                    lastDebriSpawned = i;
+                }
+            }
+            timerDebri = 0;
+
+        }
+        if (timerRock >= timeToWaitTillSpawnIncreaseRock)
+        {
+            int size = lastRockSpawned + AmountRockAddedAfterTime;
+
+            if (size > MAXNUMOFROCK)
+                size = MAXNUMOFROCK - 1;
+            for (int i = lastRockSpawned; i < size; i++)
+            {
+                if (!Resp[i].activeInHierarchy)
+                {
+                    Resp[i].SetActive(true);
+                    Vector2 Spawn = UnityEngine.Random.insideUnitCircle.normalized;
+                    SpawnDistFromPlayer.x = Spawn.x * 1200;
+                    SpawnDistFromPlayer.z = Spawn.y * 1200 - (376);
+                    Resp[i].transform.position = SpawnDistFromPlayer;
+                    lastRockSpawned = i;
+                }
+            }
+            timerAlien = 0;
+
+        }
+        if (timerAlien >= timeToWaitTillSpawnIncreaseAlien)
+        {
+            int size = lastAlienSpawned + AmountAlienAddedAfterTime;
+
+            if (size > MAXNUMOFGREENBOI)
+                size = MAXNUMOFGREENBOI - 1;
+            for (int i = lastAlienSpawned; i < size; i++)
+            {
+                if (!Resp[i].activeInHierarchy)
+                {
+                    Resp[i].SetActive(true);
+                    Vector2 Spawn = UnityEngine.Random.insideUnitCircle.normalized;
+                    SpawnDistFromPlayer.x = Spawn.x * 1200;
+                    SpawnDistFromPlayer.z = Spawn.y * 1200 - (376);
+                    Resp[i].transform.position = SpawnDistFromPlayer;
+                    lastAlienSpawned = i;
+                }
+            }
+            timerAlien = 0;
+
+        }
+
     }
-    IEnumerator Wait()
-    {
-        print(Time.time);
-        yield return new WaitForSecondsRealtime(5);
-        Respawn();
-    }
-    public void Respawn()
-    {
-        
-        
-        
-        
-    }
+
 
 }
