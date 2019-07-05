77078fd8c40bea59b94d795016e18087d29b0847
MichaelV <michael@liminalvr.com>
2019-07-05 10:42:52 +1000

    Add GameTimer script to keep track of how long the experience has been running and quit if it's been too long. Add starter script to avoid bugs with Awake function by putting scene under a parent object and turning on only once the limapp ahs loaded.

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+using Liminal.Core.Fader;
+using Liminal.SDK.Core;
+
+/// <summary>
+/// GameTimer is used to control the length of the experience
+/// </summary>
+public class GameTimer
+    : MonoBehaviour
+{
+    public float MaxGameLength;
+    public float GetTime() 
+        => _currentTime;
+
+    private float _currentTime;
+
+    // Start is called before the first frame update
+    public void StartTimer()
+    {
+        StartCoroutine(CountdownCoro());
+    }
+
+   private IEnumerator CountdownCoro()
+   {
+       while (_currentTime < MaxGameLength)
+       {
+           _currentTime += Time.deltaTime;
+
+           yield return new WaitForEndOfFrame();
+       }
+
+       ScreenFader.Instance.FadeToBlack(2);
+
+       yield return ScreenFader.Instance.WaitUntilFadeComplete();
+
+       ExperienceApp.End();
+    }
+}
